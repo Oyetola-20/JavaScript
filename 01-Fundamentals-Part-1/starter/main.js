a747d4e507d454eed1da9542f0f53d87db1e8dbc
Martinez <oyetolamateen14@gmail.com>
2024-08-14 21:11:38 -0700

    assigment

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -1,4 +1,4 @@
-// First data
+// Coding Challenge #1 and #2
 
 let markMass = 78;
 let johnMass = 92;
@@ -42,4 +42,48 @@ if (markBMI > johnBMI) {
   console.log(`markBMI is higher than johnBMI`);
 } else {
   console.log(`johnBMI is higher than markBMI`);
+}
+
+
+//Coding challenge 3
+
+const dolphinsAverageScore = (96 + 108 + 89) / 3;
+const koalasAverageScore = (89 + 91 + 110) / 3;
+
+if (dolphinsAverageScore > koalasAverageScore) {
+  console.log(`Dolphins team win`);
+}else if (koalasAverageScore > dolphinsAverageScore) {
+  console.log(`Koalas team win`);
+}else {
+  console.log(`Draw`);
+}
+
+// Data bonus 1
+
+const dolphinsAverageScore1 = (97 + 112 + 101) / 3;
+const koalasAverageScore1 = (109 + 95 + 123) / 3;
+
+if (dolphinsAverageScore1 > koalasAverageScore1 && dolphinsAverageScore1 >=100) {
+  console.log(`Dolphins team win`);
+}else if (koalasAverageScore1 > dolphinsAverageScore1 && koalasAverageScore1 >= 100){
+  console.log(`Koalas team win`);
+}else if (dolphinsAverageScore1 === koalasAverageScore1 && dolphinsAverageScore1 >=100 && koalasAverageScore1 >= 100){
+  console.log(`Draw`);
+}else{
+  console.log(`No winner`)
+}
+
+// Data bonus 2
+
+const dolphinsAverageScore2 = (97 + 112 + 101) / 3;
+const koalasAverageScore2 = (109 + 95 + 106) / 3;
+
+if (dolphinsAverageScore2 > koalasAverageScore2 && dolphinsAverageScore2 >= 100) {
+  console.log(`Dolphins team win`);
+}else if (koalasAverageScore2 > dolphinsAverageScore2 && koalasAverageScore2 >= 100){
+  console.log(`Koalas team win`);
+}else if (dolphinsAverageScore2 === koalasAverageScore2 && dolphinsAverageScore2 >=100 && koalasAverageScore2 >= 100){
+  console.log(`Draw`);
+}else{
+  console.log(`No winner`);
 }
